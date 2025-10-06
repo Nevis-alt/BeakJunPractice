d69ba64aa90418151987d42fd7099e6b9087b969
Nevis-alt <doublebackup0713@gmail.com>
2025-10-06 09:09:05 +0000

    q:

---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class MainApp
+{
+    public static void Main()
+    {
+        int inputNum = int.Parse(Console.ReadLine());
+        int min = -1;
+        if (inputNum % 3 == 0) min = inputNum / 3;
+        for (int i = 0; i < inputNum / 3; i++)
+        {
+            var buffer = inputNum - i * 3;
+            if (buffer % 5 == 0)
+            {
+                if (min == -1)
+                {
+                    min = buffer / 5 + i;
+                }
+                else if (min > buffer / 5 + i)
+                {
+                    min = buffer / 5 + i;
+                }
+            }
+        }
+        Console.WriteLine(min);
+    }
+}
